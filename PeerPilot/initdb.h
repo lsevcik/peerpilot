4d2905a63363eeb640bd8dd50c8e10debcc36ce1
Logan Sevcik <logan@sevcik.email>
2024-02-26 12:26:44 -0800

    Add qsql and initdb

---
+++ PeerPilot/initdb.h
@@ -0,0 +1,51 @@
+#include <QtSql>
+
+// TODO: Extend table schema
+const auto CLASSES_SQL = QString(R"(
+    create table classes(
+        id integer primary key,
+        title varchar not null,
+        canvas_id integer,
+        sis_id
+    )
+    )");
+
+const auto STUDENTS_SQL = QString(R"(
+    create table students(
+        id integer primary key,
+        name varchar,
+        class integer,
+        canvas_id integer,
+        sis_id integer
+    )
+)");
+
+const auto GROUPS_SQL = QString(R"(
+    create table students(
+        class_id integer,
+        student_id integer,
+        group_name varchar
+    )
+)");
+
+QSqlError initDb() {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(":memory:");
+
+    if (!db.open())
+        return db.lastError();
+
+    QStringList tables = db.tables();
+    QSqlQuery q;
+
+    if (tables.contains("classes", Qt::CaseInsensitive))
+        if (!q.exec(CLASSES_SQL))
+            return q.lastError();
+
+    if (tables.contains("students", Qt::CaseInsensitive))
+        if (!q.exec(STUDENTS_SQL))
+            return q.lastError();
+    // TODO: Add Test Data
+
+    return QSqlError();
+}
--- PeerPilot/main.cpp
+++ PeerPilot/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
+#include "initdb.h"
 
 #include <QApplication>
+#include <QtSql/qsqldatabase.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    initDb();
     w.show();
     return a.exec();
 }
