8dab6db4753464cdfdf0d1e4a954dbd584a80053
Logan Sevcik <logan@sevcik.email>
2024-03-09 20:17:49 -0800

    Add fake data generation to initdb

--- PeerPilot/initdb.h
+++ PeerPilot/initdb.h
@@ -12,21 +12,23 @@ const auto CLASSES_SQL = QString(R"(
 
 const auto STUDENTS_SQL = QString(R"(
     create table students(
-        id integer primary key,
-        name varchar,
-        class integer,
-        canvas_id integer,
-        sis_id integer
+        id INTEGER PRIMARY KEY,
+        name VARCHAR,
+        class_id INTEGER REFERENCES classes(id),
+        canvas_id INTEGER,
+        sis_id INTEGER
     )
 )");
 
 const auto GROUPS_SQL = QString(R"(
     create table groups(
-        class_id integer,
-        student_id integer,
-        group_name varchar
+        class_id INTEGER REFERENCES classes(id),
+        student_id INTEGER REFERENCES students(id),
+        name VARCHAR
     )
 )");
+
+const auto TEST_DATA = true;
 
 QSqlError initDb() {
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
@@ -38,14 +40,9 @@ QSqlError initDb() {
     QStringList tables = db.tables();
     QSqlQuery q;
 
-    if (!tables.contains("classes", Qt::CaseInsensitive)) {
+    if (!tables.contains("classes", Qt::CaseInsensitive))
         if (!q.exec(CLASSES_SQL))
             return q.lastError();
-        q.prepare("INSERT INTO classes (title) VALUES (?)");
-        q.addBindValue("Test");
-        if (!q.exec())
-            return q.lastError();
-    }
 
     if (!tables.contains("students", Qt::CaseInsensitive))
         if (!q.exec(STUDENTS_SQL))
@@ -55,7 +52,37 @@ QSqlError initDb() {
         if (!q.exec(GROUPS_SQL))
             return q.lastError();
 
-    // TODO: Add Test Data
+    if (TEST_DATA) {
+        q.prepare("INSERT INTO classes (title) VALUES (?)");
+
+        q.addBindValue("Class 1");
+        if (!q.exec())
+            return q.lastError();
+        QString class1Id = q.lastInsertId().toString();
+
+        q.addBindValue("Class 2");
+        if (!q.exec())
+            return q.lastError();
+        QString class2Id = q.lastInsertId().toString();
+
+
+        for (auto classId : {class1Id, class2Id}) {
+            for (int i = 0; i < 4; i++) {
+                q.prepare("INSERT INTO students (name, class_id) VALUES (?, ?)");
+                q.addBindValue(QString("Student ") + static_cast<char>('1' + i));
+                q.addBindValue(classId);
+                if (!q.exec())
+                    return q.lastError();
+
+                q.prepare("INSERT INTO groups (class_id, student_id, name) VALUES (?, ?, ?)");
+                q.addBindValue(classId);
+                q.addBindValue(1 + i + (classId == class2Id ? 4 : 0));
+                q.addBindValue(QString("Group ") + static_cast<char>('1' + i/2));
+                if (!q.exec())
+                    return q.lastError();
+            }
+        }
+    }
 
     return QSqlError();
 }
