ad914434154525f8216ac67ec4395c8dcab17249
Logan Sevcik <logan@sevcik.email>
2024-03-10 19:26:00 -0700

    Enable editing student roster/class name

--- PeerPilot/ui/createClass/createclass.cpp
+++ PeerPilot/ui/createClass/createclass.cpp
@@ -1,22 +1,34 @@
 #include "createClass.h"
 #include "ui_createClass.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
-createClass::createClass(QWidget* parent)
+createClass::createClass(QWidget* parent, QString className)
     : QDialog(parent)
     , ui(new Ui::createClass) {
     ui->setupUi(this);
-    q.exec("INSERT INTO classes (title) VALUES (new_class)");
-    classId = q.lastInsertId().toString();
+    q.prepare("SELECT id FROM classes WHERE title=?");
+    q.addBindValue(className);
+    q.exec();
+    if (!q.size()) {
+        q.prepare("INSERT INTO classes (title) VALUES (?)");
+        q.addBindValue(className);
+        if (!q.exec())
+            qDebug() << q.lastError();
+        classId = q.lastInsertId().toInt();
+    } else {
+        q.next();
+        classId = q.value(0).toInt();
+    }
 
-    auto model = new QSqlTableModel;
-    model->setTable("students");
-    model->select();
-    model->setFilter(QString("class=") + classId);
-    ui->studentTableView->setModel(model);
-    ui->studentTableView->hideColumn(1);
-    //studentListModel.setQuery(QString("SELECT * FROM students WHERE class=") + classId);
-    //ui->studentListView->setModel(studentListModel);
-    q.prepare(QString("INSERT INTO students (name, class) VALUES (?, ") + classId + ")");
+    ui->classNameLineEdit->setText(className);
+
+    studentListModel.setTable("students");
+    studentListModel.select();
+    studentListModel.setFilter(QString("class_id=") + QString::number(classId));
+    ui->studentTableView->setModel(&studentListModel);
+    for (auto& i : {0, 2, 3, 4})
+        ui->studentTableView->hideColumn(i);
 }
 
 createClass::~createClass() {
@@ -25,14 +37,28 @@ createClass::~createClass() {
 
 void createClass::on_manualEntryNamePushButton_clicked() {
     auto name = ui->studentNameLineEdit->text();
-    q.addBindValue(name);
-    if (!q.exec())
-        qDebug() << q.lastError();
+    ui->studentNameLineEdit->clear();
+    auto nameField = QSqlField("name", QVariant::String, "students");
+    nameField.setValue(name);
+    auto classField = QSqlField("class_id", QVariant::Int, "students");
+    classField.setValue(classId);
+    auto studentRecord = QSqlRecord();
+    studentRecord.append(nameField);
+    studentRecord.append(classField);
+    studentListModel.insertRecord(-1, studentRecord);
 }
 
 void createClass::on_savePushButton_clicked() {
-    q.prepare(QString("UPDATE classes SET title=? WHERE id=") + classId);
+    q.prepare(QString("UPDATE classes SET title=? WHERE id=") + QString::number(classId));
     q.addBindValue(ui->classNameLineEdit->text());
     q.exec();
     this->close();
+}
+
+void createClass::on_importPushButton_clicked() {
+    QFileDialog dialog(this);
+    auto fileName = QFileDialog::getOpenFileName(this,
+        tr("Open Roster"), QDir::homePath(), tr("Spreadsheets (*.xls *.xlsx *.csv)"));
+    QMessageBox::information(this, "PeerPilot", "Uninplemented :(");
+    // TODO: Import contents of class roster
 }
--- PeerPilot/ui/createClass/createclass.h
+++ PeerPilot/ui/createClass/createclass.h
@@ -13,18 +13,19 @@ class createClass : public QDialog
     Q_OBJECT
 
 public:
-    explicit createClass(QWidget *parent = nullptr);
+    explicit createClass(QWidget *parent = nullptr, QString className = "");
     ~createClass();
 
 private slots:
     void on_manualEntryNamePushButton_clicked();
     void on_savePushButton_clicked();
+    void on_importPushButton_clicked();
 
 private:
     Ui::createClass *ui;
     QSqlQuery q;
-    QSqlQueryModel studentListModel;
-    QString classId;
+    QSqlTableModel studentListModel;
+    int classId;
 };
 
 #endif // CREATECLASS_H
