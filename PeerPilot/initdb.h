150cb738e22a98b491e3b489fa53414bd1e9cb33
Logan Sevcik <logan@sevcik.email>
2024-03-27 12:48:13 -0700

    clean up database

--- PeerPilot/initdb.h
+++ PeerPilot/initdb.h
@@ -1,30 +1,22 @@
 #include <QtSql>
 
-// TODO: Extend table schema
 const auto CLASSES_SQL = QString(R"(
     CREATE TABLE classes(
         id INTEGER PRIMARY KEY,
-        title VARCHAR NOT NULL,
-        canvas_id INTEGER,
-        sis_id INTEGER
+        title VARCHAR UNIQUE NOT NULL,
+        canvas_id INTEGER
     )
     )");
 
 const auto STUDENTS_SQL = QString(R"(
     create table students(
         id INTEGER PRIMARY KEY,
-        name VARCHAR,
-        class_id INTEGER REFERENCES classes(id),
+        name VARCHAR NOT NULL,
+        class_id INTEGER REFERENCES classes(id) NOT NULL,
         canvas_id INTEGER,
-        sis_id INTEGER
-    )
-)");
-
-const auto GROUPS_SQL = QString(R"(
-    create table groups(
-        class_id INTEGER REFERENCES classes(id),
-        student_id INTEGER REFERENCES students(id),
-        name VARCHAR
+        sis_id INTEGER,
+        sis_username VARCHAR,
+        section VARCHAR
     )
 )");
 
@@ -48,10 +40,6 @@ QSqlError initDb() {
         if (!q.exec(STUDENTS_SQL))
             return q.lastError();
 
-    if (!tables.contains("groups", Qt::CaseInsensitive))
-        if (!q.exec(GROUPS_SQL))
-            return q.lastError();
-
     if (TEST_DATA) {
         q.prepare("INSERT INTO classes (title) VALUES (?)");
 
@@ -65,19 +53,12 @@ QSqlError initDb() {
             return q.lastError();
         QString class2Id = q.lastInsertId().toString();
 
+        q.prepare("INSERT INTO students (name, class_id) VALUES (?, ?)");
 
         for (auto classId : {class1Id, class2Id}) {
             for (int i = 0; i < 4; i++) {
-                q.prepare("INSERT INTO students (name, class_id) VALUES (?, ?)");
                 q.addBindValue(QString("Student ") + static_cast<char>('1' + i));
                 q.addBindValue(classId);
-                if (!q.exec())
-                    return q.lastError();
-
-                q.prepare("INSERT INTO groups (class_id, student_id, name) VALUES (?, ?, ?)");
-                q.addBindValue(classId);
-                q.addBindValue(1 + i + (classId == class2Id ? 4 : 0));
-                q.addBindValue(QString("Group ") + static_cast<char>('1' + i/2));
                 if (!q.exec())
                     return q.lastError();
             }
